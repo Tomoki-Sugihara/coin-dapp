aae222085f9b280b3db2c03d6c176c4bbb6e9960
tomoki sugihara <sugihara.10969@gmail.com>
2021-07-06 12:49:06 +0900

    Add: add torus

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,32 @@
 import type { NextPage } from 'next'
 import { Layout } from 'src/components/layout'
+import { useOpenlogin } from 'src/hooks/openlogin'
 
 const Home: NextPage = () => {
-  const handleClick = () => {
-    window.alert('Hello, World!')
+  const { openlogin, login, privateKey } = useOpenlogin()
+  const handleClick = async () => {
+    await login()
+  }
+
+  const handleClick2 = async () => {
+    console.log(openlogin?.state.privKey)
+    console.log(openlogin)
+  }
+
+  const handleLogout = async () => {
+    await openlogin?.logout({ fastLogin: false })
   }
 
   return (
     <Layout>
       <button className='p-2' onClick={handleClick}>
-        Click me!
+        login
+      </button>
+      <button className='p-2' onClick={handleClick2}>
+        confirm
+      </button>
+      <button className='p-2' onClick={handleLogout}>
+        logout
       </button>
     </Layout>
   )
