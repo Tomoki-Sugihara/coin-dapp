cf4d822db687c6bbb20ecaf5502166319a977f64
tomoki sugihara <sugihara.10969@gmail.com>
2021-07-15 12:38:39 +0900

    Add: initialToken function in token.sol

--- blockchain/contracts/Token.sol
+++ blockchain/contracts/Token.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20 {
+contract Token is ERC20, Ownable {
   uint initialSupply = 10000;
+
   constructor() ERC20("MyToken", "MT") public {
     _mint(msg.sender, initialSupply);
+  }
+
+  function initialToken(address toAddress)
+    public
+    onlyOwner
+    returns (bool successs)
+  {
+    _mint(toAddress, initialSupply);
+    return true;
   }
 }
