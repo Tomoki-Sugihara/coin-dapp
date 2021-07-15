4d61c7a5b7605e398387a60ba52dbb7365e91226
tomoki sugihara <sugihara.10969@gmail.com>
2021-07-16 08:40:32 +0900

    Success deploy function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,6 +1,6 @@
-// import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
+// import tokenArtifacts from './contracts/build/Token.json'
 import { ownerAddress, ownerPrivateKey } from './utils/config'
 import { getContract, getWeb3, runContract } from './utils/web3'
 
@@ -8,16 +8,15 @@ const miningInitialToken = async (snapshot: functions.firestore.QueryDocumentSna
   const { address } = snapshot.data()
   if (!address) throw new Error('Address is defined.')
 
-  const tokenArtifacts = require('blockchain/build/Token.json')
-
   const web3 = getWeb3()
+  const tokenArtifacts = require('./contracts/build/Token.json')
   const tokenContract = getContract(web3, tokenArtifacts, 1515)
   const abi = tokenContract.methods.initialToken(address).encodeABI()
 
   await runContract(web3, tokenContract, 1515, abi, ownerAddress, ownerPrivateKey)
 }
 
-exports.createUser = functions
+exports.createdUsersDoc = functions
   .region('asia-northeast1')
   .firestore.document('users/{userId}')
   .onCreate((snapshot, _context) => miningInitialToken(snapshot))
