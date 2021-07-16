72fed50834f74fbeb4b980729321be8229a1a216
tomoki sugihara <sugihara.10969@gmail.com>
2021-07-16 15:53:56 +0900

    Change token contract initialSupply

--- blockchain/contracts/Token.sol
+++ blockchain/contracts/Token.sol
@@ -3,19 +3,17 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20, Ownable {
-  uint initialSupply = 10000;
-
+contract Token is ERC20 {
   constructor() ERC20("MyToken", "MT") public {
-    _mint(msg.sender, initialSupply);
+    _mint(msg.sender, 10000000);
   }
 
-  function initialToken(address toAddress)
-    public
-    onlyOwner
-    returns (bool successs)
-  {
-    _mint(toAddress, initialSupply);
-    return true;
-  }
+  // function initialToken(address toAddress)
+  //   public
+  //   returns (bool successs)
+  // {
+  //   // _mint(msg.sender, 1000);
+  //   transfer(toAddress, 100);
+  //   return true;
+  // }
 }
